7efa0300ca442cf519e8abdb28a7e454d1a37ee8
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-12-30 22:36:26 +0600

    Task3

---
+++ Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    public class MySqlConnection
+    {
+        public string Data { get; set; }
+
+        private static MySqlConnection sqlConnection;
+
+        private MySqlConnection() { }
+
+        public static MySqlConnection CreateOrSqlConnection()
+        {
+            if (sqlConnection == null)
+            {
+                sqlConnection = new MySqlConnection();
+            }
+            return sqlConnection;
+        }
+    }
+}
---
+++ PersonData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    
+    class PrsonalData : CoffeeBuilder
+    {
+        public override void AddName()
+        {
+            Drink.coffee = new Name() { Name = "Arabaica" };
+        }
+
+        public override void AddLogin()
+        {
+            Drink.milk = new login() { Name = "Arabaica123123" };
+        }
+
+    }
+    abstract class CoffeeBuilder
+    {
+        public abstract void AddName();
+        public abstract void AddLogin();
+
+    }
+    class CoffeeDrink
+    {
+        public Name name { get; set; }
+        public Login login { get; set; }
+        public override string ToString()
+        {
+            string result = name.Name + Environment.NewLine +
+                login.Name + Environment.NewLine;
+            return result;
+        }
+    }
+    class Name
+    {
+        public string Name { get; set; }
+    }
+    class Login
+    {
+        public string Name { get; set; }
+    }
+
+}
